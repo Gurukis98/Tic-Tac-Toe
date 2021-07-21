301345a7af072927aed619a9f0b808abca30fe1e
Gurukis98 <84782507+Gurukis98@users.noreply.github.com>
2021-07-21 12:01:46 +0530

    Update Program.cs

--- Tic Tac Toe UC-1/Program.cs
+++ Tic Tac Toe UC-1/Program.cs
@@ -6,9 +6,9 @@ namespace Tic_Tac_Toe_UC_1
     {
        public static void Main(string[] args)
         {
+          
             char[] arr = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-            int player = 1;
 
-                }
+        }
     }
 }
