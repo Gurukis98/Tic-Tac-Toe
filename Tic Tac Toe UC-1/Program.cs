a60480dafe5c198edfbb4ba828a12e5feef24c78
Gurukis98 <84782507+Gurukis98@users.noreply.github.com>
2021-07-21 15:55:43 +0530

    [Guru]Add:Create a Board

--- Tic Tac Toe UC-1/Program.cs
+++ Tic Tac Toe UC-1/Program.cs
@@ -21,12 +21,14 @@ namespace Tic_Tac_Toe
             }
             static void Board()
             {
+                char[] arr = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
                 Console.WriteLine("  |  |  ");
-                Console.WriteLine(" {0} | {1} | {2}");
+                Console.WriteLine(" {0} | {1} | {2}",arr[1],arr[2],arr[3]);
                 Console.WriteLine("__|__|__");
-                Console.WriteLine(" {0} | {1} | {2}");
+                Console.WriteLine(" {0} | {1} | {2}", arr[4], arr[5], arr[6]);
                 Console.WriteLine("__|__|__");
-                Console.WriteLine(" {0} | {1} | {2}");
+                Console.WriteLine(" {0} | {1} | {2}", arr[7], arr[8], arr[9]);
                 Console.WriteLine("  |  |  ");
             }
         }
