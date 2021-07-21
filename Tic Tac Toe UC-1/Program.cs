e446dbfb48bb68e3c286bee9937c67c3b4017f17
Gurukis98 <kishoreguru01@gmail.com>
2021-07-21 11:47:29 +0530

    [Guru]Add:Create an Array Index

---
+++ Tic Tac Toe UC-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tic_Tac_Toe_UC_1
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            char[] arr = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            int player = 1;
+
+                }
+    }
+}
