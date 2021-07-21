2d47df782d7a00ffce70324710dcbb43041bb1e2
Gurukis98 <84782507+Gurukis98@users.noreply.github.com>
2021-07-21 12:41:45 +0530

    Update Program.cs

--- Tic Tac Toe UC-1/Program.cs
+++ Tic Tac Toe UC-1/Program.cs
@@ -8,5 +8,17 @@ namespace Tic_Tac_Toe_UC_1
         {
             char[] arr = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
         }
+        public static void UC2()
+        {
+            int player = int.Parse(Console.ReadLine());
+            if (player%2==0)
+            {
+                Console.WriteLine("Player Chance");
+            }
+            else
+            {
+                Console.WriteLine("Computer Chance");
+            }
+        }
     }
 }
